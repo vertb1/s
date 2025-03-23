9b61491f814ed3f4047179be07964f216a89645b
yael <yaelgaspar17@icloud.com>
2025-03-23 12:06:16 -0700

    Update gamesneeztheme.lua

--- gamesneeztheme.lua
+++ gamesneeztheme.lua
@@ -251,7 +251,7 @@ local ThemeManager = {} do
         groupbox:AddDropdown("FontFace", {
             Text = "Font Face",
             Default = "Code",
-            Values = {"BuilderSans", "Code", "Fantasy", "Gotham", "Jura", "Roboto", "RobotoMono", "SourceSans", "Plex"}
+            Values = {"BuilderSans", "Code", "Fantasy", "Gotham", "Jura", "Roboto", "RobotoMono", "SourceSans", "Plex", "Minecraft", "PixelService", "Arcade"}
         })
 
         
